e41175234e0f7cfc9596821e32be4073942a12bd
jungboke <tmdqjaqhdl3@naver.com>
2021-03-25 18:32:47 +0900

    0325

---
+++ javafirst/pjt/src/constructor/MainClass.java
@@ -0,0 +1,10 @@
+package constructor;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		ObjectClass obj1 = new ObjectClass(10);
+		
+	}
+}
---
+++ javafirst/pjt/src/constructor/ObjectClass.java
@@ -0,0 +1,9 @@
+package constructor;
+
+public class ObjectClass {
+	
+	public ObjectClass(int x) {
+		// TODO Auto-generated constructor stub
+	}
+	
+}
