e28eac677555d2cdd1faabbd8e43cd88b18e5524
jungboke <tmdqjaqhdl3@naver.com>
2021-03-25 16:31:24 +0900

    0325

--- javafirst/pjt_ex/src/pjt_ex/MainClass.java
+++ javafirst/pjt_ex/src/pjt_ex/MainClass.java
@@ -2,4 +2,12 @@ package pjt_ex;
 
 public class MainClass {
 
+	public static void main(String[] args) {
+		
+		Bicycle myBicycle = new Bicycle(); // new 옆에 생성자모양
+		myBicycle.color = "red";
+		myBicycle.price = 100;
+		
+		myBicycle.info();
+	}
 }
