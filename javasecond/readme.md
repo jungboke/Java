3cd89e20e7ae6f283c8c3678596a8c65c9aa644d
jungboke <tmdqjaqhdl3@naver.com>
2021-04-04 15:45:19 +0900

    0404

--- javasecond/readme.md
+++ javasecond/readme.md
@@ -29,5 +29,59 @@ servlet2 project:
 를 통한 servlet mapping 가능함.  
 2.``@WebServlet("/SE")``처럼 java annotation을 이용한 mapping도 가능함.  
   
-servlet3 project:  
+servlet4 project:  
+1.web server와 client의 request, response 과정은 httpservlet객체를 상속받은 server의 servlet으로 진행됨.  
+따라서 servlet객체를 생성하면 기본적으로 httpservlet을 상속받고 request, response객체를 만들어서 통신에 사용함.  
+2.servlet 생명주기는 @PostConstruct -> init() -> service(doGet) -> destroy() -> @PreDestroy 순으로
+진행됨. servlet class를 생성하고 그안에 @PostConstruct init() service(doGet) destroy() @PreDestroy
+를 개발자가 원하는대로 코딩함. 진행순서는 생명주기대로 진행됨.  
+  
+servlet5 project:  
+1.client가 html의 form태그를 사용하여 유저정보를 request객체로 action에 지정된 servlet으로 전송해야함.  전송방식은 doGet,
+doPost이 있고 Get은 유저정보가 url에 붙여서 전송되며, Post는 암호화되서 request객체로 전송됨.  
+servlet에서 request.getParameter 혹은 select 일때 getParameterValues 함수를 통해 전송받은 form태그 정보를
+활용할수 있음.  
+  
+jsp1 project:  
+선언태그: JSP페이지에서 Java의 멤버변수 또는 메서드를 선언
 
+		<%!
+		int num = 10;
+		String str = "jsp";
+		ArrayList<String> list = new ArrayList<String>();
+		
+		public void jspMethod(){
+			System.out.println("-- jspMethod --");
+		}
+	%>
+주석태그: JSP 주석은 JSP 파일이 서블릿파일로 변환될때 제외된다
+
+	<%-- Hello Jsp World! --%>
+스크립트릿 태그: JSP페이지에 Java코드를 넣기 위한 태그
+
+	<%
+		if(num > 0) {
+	%>
+		<p>num > 0 </p>
+	<%
+		}else{
+	%>
+		<p> num <=0 </p>
+	<%
+		}
+	%>
+표현식 태그: Java의 변수 및 메서드의 반환값을 출력하는 태그
+
+	num is <%= num %>
+지시어: 서버에서 jsp페이지를 처리하는 방법에 대한 정의
+page: 페이지 기본설정
+	
+	<%@ page language="java" contentType="text/html; charset=EUC-KR“ pageEncoding="EUC-KR"%>
+지시어
+include: include file 설정
+
+	<%@ include file=“header.jsp"%>
+지시어
+taglib: 외부라이브러리 태그 설정
+
+	<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix=“c"%>
---
+++ javasecond/servlet5/src/com/servlet/TestServletClass.java
@@ -0,0 +1,60 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServletClass
+ */
+@WebServlet("/mSignUp")
+public class TestServletClass extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServletClass() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("-- doGet() --");
+		
+		String m_name = request.getParameter("m_name");
+		String m_pass = request.getParameter("m_pass");
+		String m_gender = request.getParameter("m_gender");
+		String[] m_hobbys = request.getParameterValues("m_hobby");
+		String m_residence = request.getParameter("m_residence");
+		
+		System.out.println("m_name : " + m_name);
+		System.out.println("m_pass : " + m_pass);
+		System.out.println("m_gender : " + m_gender);
+		System.out.println("m_hobbys : " + m_hobbys);
+		System.out.println("m_residence : " + m_residence);
+		
+		Enumeration<String> names = request.getParameterNames();
+		while(names.hasMoreElements()) {
+			String name = (String) names.nextElement();
+			System.out.println("name : " + name);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("-- doPost --");
+		doGet(request, response);
+	}
+
+}
